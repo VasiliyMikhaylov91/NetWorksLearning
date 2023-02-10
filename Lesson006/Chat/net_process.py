35ceb320a3f170bd2ddf8eee27bb8917bf031c12
chelc <chelchik91@mail.ru>
2023-02-10 12:51:49 +0700

    Chat_correct_encoding

--- Lesson006/Chat/net_process.py
+++ Lesson006/Chat/net_process.py
@@ -14,31 +14,31 @@ class network:
             while self.server_run:
                 conn, addr = s.accept()
                 with conn:
-                    data = conn.recv(1).decode('ascii')
+                    data = conn.recv(1).decode('utf-8')
                     if data == 'r':
                         with open('messages.txt', 'r', encoding='utf-8') as f:
                             rec = f.read()
-                        conn.send(rec.encode('ascii'))
+                        conn.send(rec.encode('utf-8'))
                     else:
                         message = ''
                         while True:
                                 data = conn.recv(32)
                                 if not data:
                                     break
-                                message += data.decode('ascii')
+                                message += data.decode('utf-8')
                                 with open('messages.txt', 'a', encoding='utf-8') as f:
                                     f.write(message)
 
     def client_read_chat(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.server_address, 5000))
-            s.send(b'r')
+            s.send('r'.encode('utf-8'))
             rec = ''
             while True:
                 data = s.recv(1024)
                 if not data:
                     break
-                rec += data.decode('ascii')
+                rec += data.decode('utf-8')
         return rec
 
     def client_send_message(self, message: str):
